7dc7756cfd29dc1f584be807263cd9000e217321
Benoit G <benoit.giraudmissier@gmail.com>
2022-05-30 22:15:08 +0200

    Commit first file .ino
    Hello world with board

---
+++ NGL_FireBeetle_V1/NGL_FireBeetle_V1.ino
@@ -0,0 +1,18 @@
+
+*
+// Code pour NGL_Proto usiné avec ESP32 WROOM-32D-N16
+// Auetur: Benoit 
+//PINOUT PCB:
+
+Librairie ESP32 Utilisé
+
+*
+
+void setup() {
+  Serial.begin(115200);
+}
+ 
+void loop() {
+  Serial.println("Hello from DFRobot ESP-WROOM-32");
+  delay(1000);
+}
